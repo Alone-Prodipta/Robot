57afb4223f5a1dd613677afb9a12389402810dbb
Prodipta <nayebprodipta@gmail.com>
2026-05-07 23:18:02 +0530

    empty commit

--- brain.php
+++ brain.php
@@ -16,7 +16,7 @@ if (!$apiKey)
 {
     die(json_encode(["error" => ["message" => "Environment Variable GEMINI_API_KEY not found."]]));
 }
-$url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" . $apiKey;
+$url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" . $apiKey;
 
 // 3. Prepare the data payload
 $data = [
