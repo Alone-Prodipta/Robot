97f540cf4fbae1f506d8b8bbcd27cf905e107e78
Prodipta <nayebprodipta@gmail.com>
2026-05-19 21:46:03 +0530

    added the toast container

--- script.js
+++ script.js
@@ -521,4 +521,12 @@ function generateImage(prompt) {
     chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
 
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
